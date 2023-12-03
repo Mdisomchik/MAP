56c6f68c54ad456dc0d0e065fc0a16262600ef42
Mdisomchik <116945967+Mdisomchik@users.noreply.github.com>
2023-12-03 13:37:13 +0200

    Update Project.cpp
    new parts of the function

--- Project.cpp
+++ Project.cpp
@@ -7,3 +7,17 @@ const int n = 8;
 
 //first function
 bool ifSafe(const vector<vector<int>>& board, int row, int col)
+
+// same left row 
+for (int i = 0; i < col; i ++){
+if (board[row][i] == 1) {
+return false;
+   }
+}
+
+//upper left diagonal
+for (int i = row, j = col;i >= 0 && j >= 0; i--, j--){
+if (board[i][j] == 1){
+return false;
+  }
+}
