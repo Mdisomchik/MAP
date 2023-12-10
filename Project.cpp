36f970f2f68e02c5f3b2d901d3d66c3f97876f3c
Mdisomchik <116945967+Mdisomchik@users.noreply.github.com>
2023-12-11 00:09:26 +0200

    final

--- Project.cpp
+++ Project.cpp
@@ -5,17 +5,14 @@ using namespase std;
 
 const int n = 8;
 
-//first function
 bool ifSafe(const vector<vector<int>>& board, int row, int col){
-
-// same left row 
+ 
 for (int i = 0; i < col; i ++){
 if (board[row][i] == 1) {
 return false;
    }
 }
 
-//upper left diagonal
 for (int i = row, j = col;i >= 0 && j >= 0; i--, j--){
 if (board[i][j] == 1){
 return false;
@@ -51,4 +48,27 @@ return true;
 board[i][col] = 0;
 }
 }
+   return false;
+}
+
+void print(const vector<vector<int>> & board){
+
+   for (const auto & row : board){
+for (int cell : row) {
+cout << cell << " ";
+}
+cout << endl;
+   }
+}
+
+int main(){
+
+   vector<vector<int>> board(n, vector<int>(n,0));
+
+if (!colveNQueens(board,0)){
+cout << "No sollutions" << endl;
+} else {
+print(board);
+}
+return 0;
 }
