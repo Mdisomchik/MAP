81bf46994b6087a55f9fafab85241a5403158840
Mdisomchik <maxim.capinus@student.upt.ro>
2024-01-12 18:06:49 +0200

    gg

--- Project.cpp
+++ Project.cpp
@@ -1,14 +1,11 @@
-#include <Windows.h>
+#include <iostream>
 #include <vector>
-
-using namespace std;
+using namespace std;  
 
 const int n = 8;
-const int squareSize = 60;
-
-vector<int> queensPlacement(n, -1);
 
 bool isSafe(const vector<vector<int>>& board, int row, int col) {
+
     for (int i = 0; i < col; i++) {
         if (board[row][i] == 1) {
             return false;
@@ -40,86 +37,32 @@ bool solveNQueens(vector<vector<int>>& board, int col) {
             board[i][col] = 1;
 
             if (solveNQueens(board, col + 1)) {
-                queensPlacement[col] = i; 
                 return true;
             }
 
-            board[i][col] = 0; 
+            board[i][col] = 0;
         }
     }
+
     return false;
 }
 
-void drawChessboard(HDC hdc) {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            RECT square = { j * squareSize, i * squareSize, (j + 1) * squareSize, (i + 1) * squareSize };
-
-            if ((i + j) % 2 == 0) {
-                FillRect(hdc, &square, CreateSolidBrush(RGB(255, 206, 158)));
-            }
-            else {
-                FillRect(hdc, &square, CreateSolidBrush(RGB(209, 139, 71))); 
-            }
+void print(const vector<vector<int>>& board) {
+    for (const auto& row : board) {
+        for (int cell : row) {
+            cout << cell << " ";
         }
+        cout << endl;
     }
-}
-
-void drawQueens(HDC hdc, const vector<int>& queensPlacement) {
-    for (int i = 0; i < n; i++) {
-        if (queensPlacement[i] != -1) {
-            RECT queen = { queensPlacement[i] * squareSize, i * squareSize, (queensPlacement[i] + 1) * squareSize,
-                          (i + 1) * squareSize };
-            FillRect(hdc, &queen, CreateSolidBrush(RGB(255, 0, 0))); 
-        }
-    }
-}
-
-LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
-    switch (message) {
-    case WM_PAINT:
-    {
-        PAINTSTRUCT ps;
-        HDC hdc = BeginPaint(hwnd, &ps);
-
-        drawChessboard(hdc);
-        drawQueens(hdc, queensPlacement);
-
-        EndPaint(hwnd, &ps);
-    }
-    break;
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
-    default:
-        return DefWindowProc(hwnd, message, wParam, lParam);
-    }
-    return 0;
 }
 
 int main() {
     vector<vector<int>> board(n, vector<int>(n, 0));
+
     if (!solveNQueens(board, 0)) {
-        MessageBox(NULL, L"No solutions", L"Error", MB_OK | MB_ICONERROR);
-        return 1;
-    }
-
-    WNDCLASS wc = {};
-    wc.lpfnWndProc = WndProc;
-    wc.hInstance = GetModuleHandle(NULL);
-    wc.lpszClassName = L"NQueensWindowClass";
-    RegisterClass(&wc);
-
-    HWND hwnd = CreateWindow(L"NQueensWindowClass", L"N-Queens Visualization", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT,
-        CW_USEDEFAULT, n * squareSize, n * squareSize, NULL, NULL, GetModuleHandle(NULL), NULL);
-
-    ShowWindow(hwnd, SW_SHOWNORMAL);
-    UpdateWindow(hwnd);
-
-    MSG msg = {};
-    while (GetMessage(&msg, NULL, 0, 0)) {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
+        cout << "No solutions" << endl;
+    } else {
+        print(board);
     }
 
     return 0;
